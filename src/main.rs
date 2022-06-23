69071da82390d4c5bc954e2dd8924043891860a0
Nathan Lilienthal <nathan@nixpulvis.com>
2022-06-22 23:12:42 -0400

    Create our weather "Stone" device's struct.

--- src/main.rs
+++ src/main.rs
@@ -8,31 +8,65 @@ use bsp::hal;
 use hal::clock::GenericClockController;
 use hal::pac::Peripherals;
 use hal::prelude::*;
+use atsamd21g::*;
 
 mod time;
+use time::Timer;
 
+struct Stone {
+    /// Power manager
+    PM: PM,
+    /// System control
+    SYSCTRL: SYSCTRL,
+    /// Non-volitile memory controller
+    NVMCTRL: NVMCTRL,
+    /// Clock
+    CLOCK: GenericClockController,
+    /// Timer(s)
+    TIMER: Timer,
+    /// On device LED
+    LED: bsp::Led,
+}
+
+impl Stone {
+    fn new() -> Stone {
+        let mut peripherals = Peripherals::take().unwrap();
+
+        let mut clocks = GenericClockController::with_internal_32kosc(
+            peripherals.GCLK,
+            &mut peripherals.PM,
+            &mut peripherals.SYSCTRL,
+            &mut peripherals.NVMCTRL);
+
+        let gclk0 = clocks.gclk0();
+        let timer = Timer::new(peripherals.TC5, &mut clocks, &gclk0, &mut peripherals.PM);
+
+        let pins = bsp::Pins::new(peripherals.PORT);
+        let led = pins.led_sck.into();
+
+        Stone {
+            PM: peripherals.PM,
+            SYSCTRL: peripherals.SYSCTRL,
+            NVMCTRL: peripherals.NVMCTRL,
+            CLOCK: clocks,
+            TIMER: timer,
+            LED: led,
+        }
+    }
+}
 
 #[entry]
 fn main() -> ! {
-    let mut peripherals = Peripherals::take().unwrap();
-    let mut clocks = GenericClockController::with_internal_32kosc(
-        peripherals.GCLK,
-        &mut peripherals.PM,
-        &mut peripherals.SYSCTRL,
-        &mut peripherals.NVMCTRL);
-    let gclk0 = clocks.gclk0();
-    let mut timer = time::Timer::new(peripherals.TC5, &mut clocks, &gclk0, &mut peripherals.PM);
-    let mut led_last_toggled = timer.millis();
+    let mut stone = Stone::new();
+    let mut led_last_toggled = stone.TIMER.millis();
 
-    let pins = bsp::Pins::new(peripherals.PORT);
-    let mut led: bsp::Led = pins.led_sck.into();
-    led.set_high().unwrap();
+    stone.LED.set_high().unwrap();
 
     loop {
-        timer.tick();
-        let now = timer.millis();
+        stone.TIMER.tick();
+        let now = stone.TIMER.millis();
         if 500 < (now - led_last_toggled) {
-            led.toggle().unwrap();
+            stone.LED.toggle().unwrap();
             led_last_toggled = now;
         }
     }
